5e6428298a77cf8723f92273dbcb89eaed9966c5
davq23 <davidquinterogranadillo@gmail.com>
2021-06-27 18:24:41 -0400

    Kill process if not able to connect to db

--- api/database/database.js
+++ api/database/database.js
@@ -5,15 +5,16 @@ dotenv.config({
     path: './env/local.env',
 });
 
-const  { MONGODB_HOST, MONGODB_DATABASE, MONGODB_PASSWORD, MONGODB_USER } = process.env;
+const  { MONGODB_HOST, MONGODB_DATABASE, MONGODB_PASSWORD, MONGODB_PREFIX, MONGODB_USER } = process.env;
 
 mongoose.connect(
-    `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_DATABASE}?retryWrites=true&w=majority`, 
+    `${MONGODB_PREFIX}://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_DATABASE}?retryWrites=true&w=majority`, 
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
     .then(db => console.log('Database is connected'))
     .catch(err => {
-        console.error(err)
+        console.error(err);
+        process.exit(1);
     });
