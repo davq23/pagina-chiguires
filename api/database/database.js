0409ef97ac577b952bc9cbda6192b1f032423321
davq23 <davidquinterogranadillo@gmail.com>
2021-06-27 18:31:41 -0400

    use normal .env

--- api/database/database.js
+++ api/database/database.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 
 dotenv.config({
-    path: './env/local.env',
+    path: './env/.env',
 });
 
 const  { MONGODB_HOST, MONGODB_DATABASE, MONGODB_PASSWORD, MONGODB_PREFIX, MONGODB_USER } = process.env;
